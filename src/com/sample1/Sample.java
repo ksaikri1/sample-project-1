8d9724644f1b92097fe10b3fa3bed11cbd6590bc
ksaikri1 <kethasaikrishna7@gmail.com>
2020-12-19 08:07:42 -0800

    First commit & push

--- src/com/sample1/Sample.java
+++ src/com/sample1/Sample.java
@@ -3,5 +3,9 @@ package com.sample1;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Sai Krishna");
+        System.out.println("Sai Krishna");
+        System.out.println("Sai Krishna");
+        System.out.println("Sai Krishna");
+
     }
 }
